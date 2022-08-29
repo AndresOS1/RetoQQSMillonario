7fdebbae887182fdcf135734b2dd3fd3abc64c2e
Juanyonda13 <juan.yonda@misena.edu.com>
2022-08-29 14:35:26 -0500

    dashboard/admin-terminado

--- resources/views/Admin/index.blade.php
+++ resources/views/Admin/index.blade.php
@@ -37,7 +37,11 @@
                 <p>{{auth()->user()->name}}</p>
                 <i class="bi bi-person-badge-fill"></i>
             </div>
+            <div class="col-12 border-2 p-2 shadow-lg">
+
+            </div>
         </div>
+
     </section>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
