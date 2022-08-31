24ec691cca10754d177aec9b2c8a6902daea8361
Juanyonda13 <juan.yonda@misena.edu.com>
2022-08-31 08:43:39 -0500

    creacion blade de game.categories

---
+++ resources/views/Game/categories.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.nav')
+@section('content')
+     <div class="col-12 d-flex flex-column justify-content-center" >
+         <form action="">
+            <select name="" id="">
+                <option value="" selected>nieveles</option>
+            </select>
+         </form>
+         <select name="" id="">
+            <option value="" selected>categorias</option>
+         </select>
+         <select name="" id="">
+            <option value="" selected>pregunta</option>
+         </select>
+         <select name="" id="">
+            <option value="" selected>respuestas</option>
+        </select>
+     </div>
+@endsection
