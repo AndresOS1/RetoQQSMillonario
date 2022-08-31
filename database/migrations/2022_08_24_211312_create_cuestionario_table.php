1c3523296149a2b0fce903624e42e3d2f1be0bae
Juanyonda13 <juan.yonda@misena.edu.com>
2022-08-31 08:25:31 -0500

    casi todas las foranes(user,categorias,niveles,respuestas,preguntas) en la tabla game

--- database/migrations/2022_08_24_211312_create_cuestionario_table.php
+++ database/migrations/2022_08_24_211312_create_cuestionario_table.php
@@ -18,12 +18,17 @@ return new class extends Migration
 
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('categoria_id');
+            $table->unsignedBigInteger('niveles_id');
             $table->unsignedBigInteger('respuesta_id');
-           
+            $table->unsignedBigInteger('preguntas_id');
+      
 
             $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('niveles_id')->references('id_niveles')->on('niveles')->onDelete('cascade')->onUpdate('cascade');
             $table->foreign('respuesta_id')->references('id_respuestas')->on('respuestas');
             $table->foreign('categoria_id')->references('id_categorias')->on('categorias');
+            $table->foreign('preguntas_id')->references('id_preguntas')->on('preguntas')->onDelete('cascade')->onUpdate('cascade');
+
 
             $table->timestamps();
         });
