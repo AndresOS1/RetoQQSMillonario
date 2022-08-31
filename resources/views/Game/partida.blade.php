02bbfc22d630cd90c5e04c4f521ff269fffdc895
Juanyonda13 <juan.yonda@misena.edu.co>
2022-08-31 07:30:36 -0500

    vista de las pregunta con el consumo de la pregunta

--- resources/views/Game/partida.blade.php
+++ resources/views/Game/partida.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.nav')
 @section('content')
+
      <div class="col-12 d-flex flex-column">
+@if($preguntaAleatoria->count())
         <section class="col-12 d-flex justify-content-center aligin-items-center flex-wrap mt-5">
             <div class="casilla col-12 img-fluid d-flex justify-content-center text-break">
                  <div class="text-breack p-4">
@@ -8,4 +10,10 @@
                  </div>
             </div>
      </div>
+     @else
+     <h1>No hay preguntas registradas</h1>
+     @role('admin')
+         <a href="{{route('Preguntas.create')}}" class="btn">ir a crear preguntas</a>
+     @endrole
+@endif
 @endsection
